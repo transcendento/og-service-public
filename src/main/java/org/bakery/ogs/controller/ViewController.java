5b7d0318dce8c725b92bfc8bf3f0a264e4b10116
maxim <kammis@mail.ru>
2023-11-13 01:46:35 +0500

    @PutMapping, @DeleteMapping

--- src/main/java/org/bakery/ogs/controller/ViewController.java
+++ src/main/java/org/bakery/ogs/controller/ViewController.java
@@ -8,11 +8,18 @@ import org.bakery.ogs.model.AuthView;
 import org.bakery.ogs.model.ContentView;
 import org.bakery.ogs.model.DaysView;
 import org.bakery.ogs.repository.AuthViewRepository;
+import org.bakery.ogs.repository.ContentSaveRepository;
 import org.bakery.ogs.repository.ContentViewRepository;
 import org.bakery.ogs.repository.DaysViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +35,9 @@ public class ViewController {
 
 	@Autowired
 	ContentViewRepository contentViewRepository;
+
+	@Autowired
+	ContentSaveRepository contentSaveRepository;
 
 	@GetMapping("/ogs/{tokenall}/auth")
 	public List<AuthView> getAuthByTokenAll(@PathVariable(value = "tokenall") String tokenAll,
@@ -46,6 +56,18 @@ public class ViewController {
 	public List<ContentView> getContentById(@PathVariable(value = "id") String daysId, HttpServletResponse response) {
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		return contentViewRepository.findByDaysordermoveidstr(daysId);
+	}
+
+	@PutMapping("/ogs/prod")
+	public ResponseEntity<ContentView> putContent(@RequestBody List<ContentView> listContent) {
+		contentSaveRepository.saveAll(listContent);
+		return new ResponseEntity<ContentView>(HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/ogs/prod/del")
+	public ResponseEntity<ContentView> delContent() {
+		contentSaveRepository.deleteAll();
+		return new ResponseEntity<ContentView>(HttpStatus.CREATED);
 	}
 
 }
--- src/main/java/org/bakery/ogs/security/config/SecurityConfig.java
+++ src/main/java/org/bakery/ogs/security/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.bakery.ogs.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -35,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private JwtAuthenticationEntryPoint unauthorizedHandler;
 
     @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+    JwtAuthenticationFilter jwtAuthenticationFilter() {
         return new JwtAuthenticationFilter();
     }
 
@@ -53,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
